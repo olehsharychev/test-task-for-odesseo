d5205fc48ec0a7831c569a9072efa41e0c376f41
Xike121 <shar1997@ukr.net>
2020-09-03 23:55:08 +0300

    adding and deleting records added

--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,65 @@ let surnameName = document.getElementById("surname-name");
 let position = document.getElementById("position");
 let email = document.getElementById("email");
 let tel = document.getElementById("tel");
+let editBtns = document.getElementsByClassName("list-item__btn--edit");
+let deleteBtns = document.getElementsByClassName("list-item__btn--delete");
+let list = document.querySelector(".list");
 
-function addNode(node) {
-    
+if(localStorage.length > 0) {
+    arrEmployees = JSON.parse(localStorage.employeesStr);
 }
 
-function deleteNode(node) {
-    
+function renderList() {
+    let htmlContent = "";
+    let i = 0;
+    arrEmployees.forEach(function (item) {
+         htmlContent += `
+            <div id="${i}" class="list__item list-item">
+                <h4 class="list-item__surname-name">${item.surnameName}</h4>
+                <p class="list-item__position">${item.position}</p>
+                <p class="list-item__email-tel">Email: ${item.email} | Номер телефона: ${item.tel}</p>
+                <button class="list-item__btn list-item__btn--edit"></button>
+                <button class="list-item__btn list-item__btn--delete"></button>
+            </div>
+        `;
+        i++;
+    });
+    list.innerHTML = htmlContent;
+    for (let i = 0; i < editBtns.length; i++) {
+        editBtns[i].addEventListener("click", editRecord);
+        deleteBtns[i].addEventListener("click", deleteRecord);
+    }
 }
 
-function editNode(node) {
+function clearForm() {
+    employeeForm.reset();
+}
+
+function saveRecords() {
+    if (arrEmployees.length === 0) {
+        localStorage.clear();
+    }
+    else {
+        let recordsStr = JSON.stringify(arrEmployees);
+        localStorage.setItem("employeesStr", recordsStr);
+    }
+}
     
+function addRecord(record) {
+    arrEmployees.push(record);
+    renderList();
+    clearForm();
+}
+
+function deleteRecord() {
+    let itemId = this.parentNode.id;
+    arrEmployees.splice(itemId, 1);
+    renderList();
+}
+
+function editRecord() {
+    console.log(this.parentNode.id);
+    console.log("edit");
 }
 
 function checkValidity() {
@@ -54,7 +102,21 @@ function checkValidity() {
     return succeed;
 }
 
+renderList();
+
 employeeForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    checkValidity();
+    if (checkValidity()){
+        let record = {
+            surnameName : surnameName.value,
+            position : position.value,
+            email : email.value,
+            tel : tel.value
+        };
+        addRecord(record);
+    }
 });
+
+window.onbeforeunload = function () {
+    saveRecords();
+};
